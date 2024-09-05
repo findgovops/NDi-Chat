406db3c2b7c4edf3275534c58155c9ac69e10218
Parker <Parker.Randolph@netdes.com>
2024-09-05 17:32:31 -0400

    updating pal chat extension

--- src/features/chat-page/chat-services/chat-api/chat-api-dynamic-extensions.ts
+++ src/features/chat-page/chat-services/chat-api/chat-api-dynamic-extensions.ts
@@ -86,11 +86,11 @@ async function executeFunction(props: {
     const headerItems = await Promise.all(headerPromise);
 
     // we need to add the user id to the headers as this is expected by the function and does not have context of the user
-    // headerItems.push({
-    //   id: "authorization",
-    //   key: "authorization",
-    //   value: await userHashedId(),
-    // });
+    headerItems.push({
+      id: "authorization",
+      key: "authorization",
+      value: await userHashedId(),
+    });
     // map the headers to a dictionary
     const headers: { [key: string]: string } = headerItems.reduce(
       (acc: { [key: string]: string }, header) => {
@@ -123,13 +123,9 @@ async function executeFunction(props: {
 
     const response = await fetch(functionModel.endpoint, requestInit);
 
-
-  if (!response.ok) {
-    console.error('API call failed:', response.status, response.statusText);
-    console.error('Request URL:', functionModel.endpoint);
-    console.error('Request Headers:', headers);
-    return `There was an error calling the api: ${response.statusText}`;
-  }
+    if (!response.ok) {
+      return `There was an error calling the api: ${response.statusText}`;
+    }
     const result = await response.json();
 
     return {
