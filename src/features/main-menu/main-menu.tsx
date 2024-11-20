e94b6c0e0b2407185023289972413bac1de18655
Parker <Parker.Randolph@netdes.com>
2024-11-19 19:37:16 -0500

    updated main page

--- src/features/main-menu/main-menu.tsx
+++ src/features/main-menu/main-menu.tsx
@@ -56,7 +56,7 @@ export const MainMenu = async () => {
               <BookOpenText {...menuIconProps} />
             </MenuLink>
           </MenuItem>
-          {user.email === process.env.ADMIN_EMAIL_ADDRESS && (
+          {user.isAdmin || process.env.ADMIN_EMAIL_ADDRESS && (
             <MenuItem tooltip="access Portal">
               <MenuLink href="/admin" ariaLabel="Go to the Access Portal">
                 <Shield {...menuIconProps} />
